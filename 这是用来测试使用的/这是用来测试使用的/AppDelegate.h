0d4b29855950634997d97b2c70af9d3b8319df27
王志准 <1986260711@qq.com>
2016-06-24 20:00:27 +0800

    初始版本

---
+++ 这是用来测试使用的/这是用来测试使用的/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  这是用来测试使用的
+//
+//  Created by 王志准 on 16/6/24.
+//  Copyright © 2016年 王志准. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
